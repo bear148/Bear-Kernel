8e3d8cae78ebab1485294691992bf9191205cfa5
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-28 22:48:16 -0700

    Extra Thing

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,8 +4,11 @@ void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("Welcome to Bear OS!\n");
-    print_str("This is a message!\n");
     print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("Version: 0.1.3\n");
+    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+    print_str("This is a message!\n");
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     print_str("Bears are the best! So is C++!");
     print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_GREEN);
     print_str("This OS is in the VERY VERY VERY early stages. It is no where near being finished.");
