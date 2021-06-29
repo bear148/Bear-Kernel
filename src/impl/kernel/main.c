b6156788ee15f7aef9714d0a17e08ef9c3f9ab63
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-29 05:57:04 -0700

    Changed Startup Screen; removed some text

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -6,10 +6,12 @@ void kernel_main() {
     print_str("Welcome to Bear OS!\n");
     print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
     print_str("Version: 0.1.3\n");
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("This is a message!\n");
-    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
-    print_str("Bears are the best! So is C++!");
-    print_set_color(PRINT_COLOR_BLUE, PRINT_COLOR_GREEN);
-    print_str("This OS is in the VERY VERY VERY early stages. It is no where near being finished.");
+    print_set_color(PRINT_COLOR_BLACK, PRINT_COLOR_BLACK);
+    print_str("\n");
+    print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_BLACK);
+    print_str("To-Do\n");
+    print_str("- Custom terminal\n");
+    print_str("- Main Menu\n");
+    print_str("- Some sort of UI\n");
+    print_str("- Cursor?\n");
 }
